8e82d7f2e095c69cb73afe1f712fd3338f6900db
Albano Alice <alice.albano@albano_alice@yahoo.fr>
2017-02-25 20:39:57 +0100

    [DEV] Implement SymbolString class
    This class handles errors if we give an unknown symbol

--- src/lcd/SymbolString.java
+++ src/lcd/SymbolString.java
@@ -6,8 +6,34 @@ public class SymbolString {
 	private Symbol[] symbolstring;
 	
 	public SymbolString(String symbolString) {
-		//initialise symbolstring a partir d'une chaine
-		//Ici, gestion d'erreur sur la chaine a encoder (caractère inconnu)		
+		symbolstring = new Symbol[symbolString.length()];
+
+	    for (int x = 0; x < symbolString.length(); x++) {
+	    	
+	    	switch (symbolString.charAt(x)) {
+	    		case '0':	symbolstring[x] = Symbol.ZERO;
+	    					break;
+	    		case '1':	symbolstring[x] = Symbol.ONE;
+							break;
+	    		case '2':	symbolstring[x] = Symbol.TWO;
+							break;
+	    		case '3':	symbolstring[x] = Symbol.THREE;
+							break;
+	    		case '4':	symbolstring[x] = Symbol.FOUR;
+							break;
+	    		case '5':	symbolstring[x] = Symbol.FIVE;
+							break;
+	    		case '6':	symbolstring[x] = Symbol.SIX;
+							break;
+	    		case '7':	symbolstring[x] = Symbol.SEVEN;
+							break;
+	    		case '8':	symbolstring[x] = Symbol.EIGHT;
+							break;
+	    		case '9':	symbolstring[x] = Symbol.NINE;
+							break;
+				default:	throw new IllegalArgumentException();
+	    	}
+	    }
 	
 	}
 	
