2df5d17eadbf7b7e468c5c7c1f5d289941cbd9ba
Albano Alice <alice.albano@albano_alice@yahoo.fr>
2017-02-26 16:49:55 +0100

    [DEV] Implement the ControllerLCD class
    This class will be test using black box testing.

--- src/lcd/ControllerLCD.java
+++ src/lcd/ControllerLCD.java
@@ -2,28 +2,33 @@ package lcd;
 
 
 public class ControllerLCD {
-	
-	private SymbolString symbolString;
+
+	private Grid[] gridArray;
 	private LCDScreen screen;
 	
 	public ControllerLCD() {
 		this.screen = new LCDScreen();
 	}
 	
+	//We test this method and the printLCDScreen method by black box testing
 	public void printSymbolString(SymbolString symbolString) {
-
-		//call getSymbolString 
 		
-		//call Grid on each symbol and call setLCDScreen 
-		//call printScreen
+		int lengthString = symbolString.getSymbolString().length;
+		
+		int lengthGridArray = Math.min(lengthString, screen.getWidth());
+		gridArray = new Grid[lengthGridArray];
+		
+		int i;
+		
+		// If the screen to print is to long for the screen, we only print the
+		// number of characters as the width of the screen
+		for (i = 0; ((i < lengthString) && (i < screen.getWidth())) ; i ++) {
+			gridArray[i] = new Grid(symbolString.getSymbolString()[i]);
+		}
+		
+		screen.setLCDScreen(gridArray);
+
+		screen.printLCDScreen();
 	}
-	
-	private Grid[] computeResult() {
-		//call encoder
-		return null;
-	}
-	
-	private void printResult() {
-		//call printScreen
-	}
+
 }
