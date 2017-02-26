88b4ede62fbae10510029f79d47983cad5e7392f
Albano Alice <alice.albano@albano_alice@yahoo.fr>
2017-02-26 02:05:19 +0100

    [DEV] Implement the LCDScreen class
    The printLCDScreen method is not yet implemented (for now, we won't test it)

--- src/lcd/LCDScreen.java
+++ src/lcd/LCDScreen.java
@@ -1,19 +1,43 @@
 package lcd;
 
-//On suppose que notre écran utilisé ici fait 10 de large
+// We assume that our screen can print 10 numbers (width = 10)
 public class LCDScreen extends Screen {
 	
 	private int width = 10;
 	
 	private Grid[] screen = new Grid[width];
 	
-	public void setLCDScreen(Grid[] grid) {
-		//Remplit les grids de l'écran avec des symboles encodés
-		//Fill with empty grid for print
+	
+// This method will be called by the ControllerLCD, which already 
+// checks if the length of the array grid is <= width
+// Therefore we do not handle this case in this method
+	public void setLCDScreen(Grid[] gridArray) {
+		int lengthGridArray = gridArray.length;
+		int i = 0;
+		
+		// If we don't want to print 10 characters, we add empty grids to the screen
+		// It will allow to print to the right of the screen
+		if (lengthGridArray < 10) {
+			int diff = 10 - lengthGridArray;
+			Grid emptyGrid = new Grid(Symbol.EMPTY);
+			
+			while (diff > 0) {
+				this.screen[i] = emptyGrid; 
+				diff --;
+				i ++;
+			}
+		}	
+		
+		// We add the grids given in parameter to our screen (on the right)
+		int j;
+		for (j = 0; j < lengthGridArray; j ++) {
+			this.screen[i] = gridArray[j];
+			i ++;	
+		}
 	}
 	
 	public Grid[] getLCDScreen() {
-		return null;
+		return this.screen;
 	}
 	
 	public void printLCDScreen() {
